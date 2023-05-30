9794bdac479398f50bd40930278ad04d25b7de4b
praveen123 <gunnampraveenreddy@gmail.com>
2023-05-30 11:38:59 +0530

    To see the metadata of a pandas Dataframes

--- Pandas_DataFrames.py
+++ Pandas_DataFrames.py
@@ -5,27 +5,27 @@ import pandas as pd
 titanic_df=pd.read_csv('titanic.csv')
 print(titanic_df)
 
-# # datatype of each series on dataframe , here series means column
-# titanic_df=titanic_df.dtypes # display column with data type
-# print(titanic_df)
+# datatype of each series on dataframe , here series means column
+titanic_df=titanic_df.dtypes # display column with data type
+print(titanic_df)
 
-# # display each column count, mean, std, min, max , etc values based on datatype 
-# titanic_df=titanic_df.describe()
-# print(titanic_df)
+# display each column count, mean, std, min, max , etc values based on datatype 
+titanic_df=titanic_df.describe()
+print(titanic_df)
 
 # display count, mean, max, min etc values on particular column
 titanic_df=titanic_df['Age'].describe()
 print(titanic_df)
 
-# # display metadata of a DataFrame
-# titanic_df=titanic_df.info()
-# print(titanic_df)
+# display metadata of a DataFrame
+titanic_df=titanic_df.info()
+print(titanic_df)
 
-# # display only object datatype columns
-# df_dtype=titanic_df.dtypes[titanic_df.dtypes == 'object'].index
-# print(df_dtype)
+# display only object datatype columns
+df_dtype=titanic_df.dtypes[titanic_df.dtypes == 'object'].index
+print(df_dtype)
 
-# # display metadata of particular datatype of columns
-# df_dtype=titanic_df.dtypes[titanic_df.dtypes == 'object'].index
-# titanic_df=titanic_df[df_dtype].describe()
-# print(titanic_df)
+# display metadata of particular datatype of columns
+df_dtype=titanic_df.dtypes[titanic_df.dtypes == 'object'].index
+titanic_df=titanic_df[df_dtype].describe()
+print(titanic_df)
